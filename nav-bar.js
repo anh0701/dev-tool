0783cb9e0d0895ef52153077b1e4e6419bcf5bf1
Pham Thi Quynh Anh <quynhanhphamthi01@gmail.com>
2026-08-26 16:17:10 +0700

    active navbar

--- nav-bar.js
+++ nav-bar.js
@@ -10,3 +10,18 @@ menuToggle.addEventListener("click", () => {
         menuToggle.textContent = "☰";
     }
 });
+
+// active button 
+
+const currentPage = window.location.pathname.split("/").pop() || "index.html";
+
+    document.querySelectorAll(".menu a").forEach(link => {
+        const linkPage = link.getAttribute("href");
+
+        if (
+            linkPage === currentPage ||
+            (currentPage === "" && linkPage === "./")
+        ) {
+            link.classList.add("active");
+        }
+    });
